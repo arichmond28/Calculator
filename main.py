f17afc5ab35d972ea1dc19b46f011591eacd701c
Andrew Richmond <ar744@cornell.edu>
2024-05-20 12:59:06 -0400

    Implemented multiplication and division operators.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
   user_input1 = input("Enter a number: ")
-  operation = input("Enter an operation (Plus, Minus): ")
+  operation = input("Enter an operation (Plus, Minus, Multiply, Divide): ")
   user_input2 = input("Enter another number: ")
   if operation == "Plus":
     print(int(user_input1) + int(user_input2))
   elif operation == "Minus":
     print(int(user_input1) - int(user_input2))
+  elif operation == "Multiply":
+    print(int(user_input1) * int(user_input2))
+  elif operation == "Divide":
+    print(int(user_input1) / int(user_input2))  
   else:
     print("Invalid operation")
 main()  
